3285b150932e7279e2af9d589d2995b3c93457f3
andi0b <andix25@gmail.com>
2020-12-02 10:46:51 +0100

    Day 2

---
+++ src/Day02.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace aoc_runner
+{
+    public class Day02
+    {
+        public int Part1(string[] inputs)
+            => inputs.Select(ParseInput)
+                     .Count(x => IsPasswordValid(x.password, x.policy));
+
+        public int Part2(string[] inputs)
+            => inputs.Select(ParseInput)
+                     .Count(x => IsPasswordValid2(x.password, x.policy));
+        
+        public bool IsPasswordValid(string password, PasswordPolicy policy)
+        {
+            var policyCharCount = password.ToCharArray().Count(c => c == policy.Char);
+            return policyCharCount >= policy.Min && policyCharCount <= policy.Max;
+        }
+
+        public bool IsPasswordValid2(string password, PasswordPolicy policy)
+            => password[policy.Min-1] == policy.Char ^ password[policy.Max-1] == policy.Char;
+
+        public (string password, PasswordPolicy policy) ParseInput(string input)
+        {
+            var match = new Regex(@"(\d*)-(\d*) (\w): (.*)").Match(input);
+            
+            return (match.GetGroupValue<string>(4),
+                new PasswordPolicy(match.GetGroupValue<char>(3),
+                                   match.GetGroupValue<int>(1),
+                                   match.GetGroupValue<int>(2)));
+        }
+    }
+
+    public record PasswordPolicy (char Char, int Min, int Max);
+}
---
+++ tests/Day02Test.cs
@@ -0,0 +1,28 @@
+﻿using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day02Test
+    {
+        private readonly string[] _sampleInput =
+        {
+            "1-3 a: abcde",
+            "1-3 b: cdefg",
+            "2-9 c: ccccccccc",
+        };
+        
+        [Fact] Day02 GetInstance() => new();
+        [Fact] void Part1() => GetInstance().Part1(_sampleInput).Should().Be(2);
+        [Fact] void Part2() => GetInstance().Part2(_sampleInput).Should().Be(1);
+        
+        [Theory,
+         InlineData("1-3 a: abcde", "abcde", 'a', 1, 3),
+         InlineData("1-3 b: cdefg", "cdefg", 'b', 1, 3),
+         InlineData("2-9 c: ccccccccc", "ccccccccc", 'c', 2, 9)
+        ]
+        void Parse(string input, string password, char policyChar, int min, int max)
+            => GetInstance().ParseInput(input).Should().BeEquivalentTo((password, new PasswordPolicy(policyChar, min, max)));
+    }
+}

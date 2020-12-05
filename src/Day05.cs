fdcb3fa34f5f7c8d1e1c9017d931a9c037b4b9d5
andi0b <andix25@gmail.com>
2020-12-05 08:50:57 +0100

    Day 5 Part 1

---
+++ src/Day05.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace aoc_runner
+{
+    public class Day05
+    {
+        public int Part1(string[] input) =>
+            input.Select(BoardingPass.Parse).Max(x => x.SeatId);
+        
+        public record BoardingPass(int Row, int Column)
+        {
+            public int SeatId => 8 * Row + Column;
+
+            public static BoardingPass Parse(string input) =>
+                new(ParseBinary(input[..7]), ParseBinary(input[7..]));
+
+            static int ParseBinary(string str) =>
+                Convert.ToInt32(str.Replace('F', '0').Replace('B', '1').Replace('R', '1').Replace('L', '0'), 2);
+        }
+    }
+}
---
+++ tests/Day05Test.cs
@@ -0,0 +1,21 @@
+﻿using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day05Test
+    {
+        [Theory,
+         InlineData("FBFBBFFRLR", 44, 5, 357),
+         InlineData("BFFFBBFRRR", 70, 7, 567),
+         InlineData("FFFBBBFRRR", 14, 7, 119),
+         InlineData("BBFFBBFRLL", 102, 4, 820),
+        ]
+        void CheckParse(string input, int row, int col, int seatId)
+            => Day05.BoardingPass.Parse(input).Should().BeEquivalentTo(new
+            {
+                Row = row, Column = col, SeatId = seatId
+            });
+    }
+}

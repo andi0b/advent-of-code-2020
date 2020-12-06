a27a894b97b898798d81d35062e707d9683101d8
andi0b <andix25@gmail.com>
2020-12-06 08:33:50 +0100

    Day 6

---
+++ src/Day06.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc_runner
+{
+    public record Day06(Day06.Group[] Groups)
+    {
+        public Day06(string input):this(System.Array.Empty<Group>())
+        {
+            var inputGroups = input.Split(Environment.NewLine + Environment.NewLine);
+            Groups = (from g in inputGroups
+                      select new Group(g.Split(Environment.NewLine))).ToArray();
+        }
+
+        public int Part1() => Groups.Sum(g => g.Union.Length);
+        public int Part2() => Groups.Sum(g => g.Intersect.Length);
+        
+        public record Group(string[] CustomsDeclarations)
+        {
+            public string Intersect => Aggregate(Enumerable.Intersect);
+            public string Union => Aggregate(Enumerable.Union);
+            private string Aggregate(Func<IEnumerable<char>,IEnumerable<char>, IEnumerable<char>> func) =>
+                CustomsDeclarations.Aggregate((agg, next) => new string(func(agg, next).ToArray()));
+        }
+    }
+}
---
+++ tests/Day06Test.cs
@@ -0,0 +1,29 @@
+﻿using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day06Test
+    {
+        private string demoInput = @"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+
+        [Fact] public Day06 GetInstance() => new (demoInput);
+        [Fact] void Part1() =>  GetInstance().Part1().Should().Be(11);
+        [Fact] void Part2() =>  GetInstance().Part2().Should().Be(6);
+    }
+}

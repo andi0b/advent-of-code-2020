1e289e34778b54d49167feab3daa5616d227d826
andi0b <andix25@gmail.com>
2020-12-06 10:39:43 +0100

    undo day 5 formatting

--- src/Day05.cs
+++ src/Day05.cs
@@ -3,25 +3,27 @@ using System.Linq;
 
 namespace aoc_runner
 {
-public record Day05(int[] TakenSeatIds)
-{
-    public Day05(string[] input) : this( input.Select(ParseSeatId).ToArray()) {}
+    public record Day05(int[] TakenSeatIds)
+    {
+        public Day05(string[] input) : this(input.Select(ParseSeatId).ToArray())
+        {
+        }
 
-    public int Part1() => TakenSeatIds.Max();
+        public int Part1() => TakenSeatIds.Max();
 
-    public int Part2()
-        => Enumerable.Range(0, TakenSeatIds.Max()) // all possible seats IDs (including empty front rows, excluding last taken one)
-                     .Except(TakenSeatIds)         // except the taken seats
-                     .Max();                       // my seat is the free seat with the highest id
-                                                   // (the others are empty front row seats)
+        public int Part2()
+            => Enumerable.Range(0, TakenSeatIds.Max()) // all possible seats IDs (including empty front rows, excluding last taken one)
+                         .Except(TakenSeatIds)         // except the taken seats
+                         .Max();                       // my seat is the free seat with the highest id
+        // (the others are empty front row seats)
 
-    public static int ParseSeatId(string boardingPass) => Convert.ToInt32(ConvertToBinary(boardingPass), 2);
+        public static int ParseSeatId(string boardingPass) => Convert.ToInt32(ConvertToBinary(boardingPass), 2);
 
-    public static string ConvertToBinary(string boardingPass) =>
-        boardingPass
-           .Replace('F', '0')
-           .Replace('B', '1')
-           .Replace('L', '0')
-           .Replace('R', '1');
-}
+        public static string ConvertToBinary(string boardingPass) =>
+            boardingPass
+               .Replace('F', '0')
+               .Replace('B', '1')
+               .Replace('L', '0')
+               .Replace('R', '1');
+    }
 }

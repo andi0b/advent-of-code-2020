76fb43fe2bdd10e126f3dc6d9eb851ae71242710
andi0b <andix25@gmail.com>
2020-12-07 11:30:33 +0100

    Day 7 optimizations

--- src/Day07.cs
+++ src/Day07.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -7,45 +7,43 @@ namespace aoc_runner
 {
     public record Day07(Day07.BagRule[] BagRules) 
     {
-        public Day07(string[] input) :this (input.Select(BagRule.Parse).ToArray()){}
-        
+        public Day07(string[] input) : this (input.Select(BagRule.Parse).ToArray()){}
+
         public int Part1()
         {
-            var lookup = (
+            var childParentLookup = (
                 from rule in BagRules
-                from contains in rule.Contains
-                select (contains.color, rule)
-            ).ToLookup(x => x.color, x => x.rule);
+                from childBag in rule.Contains
+                select (childBag, rule)
+            ).ToLookup(x => x.childBag.color, x => x.rule.Color);
 
-            return AllParentBags("shiny gold").Count();
+            return ParentBagColors("shiny gold").Count();
 
-            IEnumerable<BagRule> AllParentBags(string color) =>
-                lookup![color].Concat(
-                                   lookup![color].Select(r => r.Color).SelectMany(AllParentBags)
-                               )
-                              .Distinct();
+            IEnumerable<string> ParentBagColors(string color) =>
+                childParentLookup[color].SelectMany(ParentBagColors)
+                                        .Distinct()
+                                        .Concat(childParentLookup[color]);
         }
 
         public int Part2()
         {
-            var bagDict = BagRules.ToDictionary(x => x.Color, x => x);
+            var rulesByColor = BagRules.ToDictionary(x => x.Color, x => x);
 
-            return ContainingBagsNum("shiny gold") - 1;
+            return TotalBagCount("shiny gold") - 1;
 
-            int ContainingBagsNum(string color)
-                => 1 + bagDict[color].Contains.Sum(x => x.num * ContainingBagsNum(x.color));
+            int TotalBagCount(string color)
+                => 1 + rulesByColor[color].Contains.Sum(x => x.count * TotalBagCount(x.color));
         }
 
-        public record BagRule(string Color, (string color, int num)[] Contains)
+        public record BagRule(string Color, (string color, int count)[] Contains)
         {
-            public static BagRule Parse(string input) =>
-                new(
-                    Color: Regex.Match(input, @"^(\S+ \S+)").Groups[0].Value,
-                    Contains: (
-                        from match in Regex.Matches(input, @"(\d+) (\S+ \S+)")
-                        select (match.Groups[2].Value, int.Parse(match.Groups[1].Value))
-                    ).ToArray()
-                );
+            public static BagRule Parse(string input) => new(
+                Color: Regex.Match(input, @"^(\S+ \S+)").Groups[0].Value,
+                Contains: (
+                    from match in Regex.Matches(input, @"(\d+) (\S+ \S+)")
+                    select (match.Groups[2].Value, int.Parse(match.Groups[1].Value))
+                ).ToArray()
+            );
         }
     }
 }

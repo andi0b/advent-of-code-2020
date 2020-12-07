6a6f562cce4894aa7e5e514c293f4c2f9bc8a342
andi0b <andix25@gmail.com>
2020-12-07 10:28:41 +0100

    Day 7

---
+++ src/Day07.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace aoc_runner
+{
+    public record Day07(Day07.BagRule[] BagRules) 
+    {
+        public Day07(string[] input) :this (input.Select(BagRule.Parse).ToArray()){}
+
+        public int Part1() => AllParentBags("shiny gold").Count();
+        
+        IEnumerable<BagRule> AllParentBags(string color)
+        {
+            var directParentBags = DirectParentBags(color).ToArray();
+            return directParentBags.Concat(directParentBags.Select(r => r.Color).SelectMany(AllParentBags)).Distinct();
+        }
+
+        IEnumerable<BagRule> DirectParentBags(string color)
+            => BagRules.Where(r => r.CanContain.Any(x => x.color == color));
+
+        public int Part2()
+        {
+            var bagDict = BagRules.ToDictionary(x => x.Color, x => x);
+
+            return ContainingBagsNum("shiny gold") - 1;
+
+            int ContainingBagsNum(string color)
+                => 1 + bagDict[color].CanContain.Sum(x => x.num * ContainingBagsNum(x.color));
+        }
+
+        public record BagRule(string Color, (string color, int num)[] CanContain)
+        {
+            private static Regex Regex = new(@"(\d+) (\S+ \S+)");
+            public static BagRule Parse(string input)
+            {
+                var parts = input.TrimEnd('.').Split("contain");
+                var color = parts[0].Replace("bags", "").Trim();
+
+                var canContain =
+                    from numberedBag in parts[1].Split(',')
+                    let match = Regex.Match(numberedBag)
+                    where match.Success
+                    select (match.GetGroupValue(2), match.GetGroupValue<int>(1));
+
+                return new BagRule(color, canContain.ToArray());
+            }
+        }
+    }
+}
---
+++ tests/Day07Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xunit;
+using aoc_runner;
+using FluentAssertions;
+using static aoc_runner.Day07;
+
+namespace tests
+{
+    public class Day07Test
+    {
+        private static string[] _sampleInput = new[]
+        {
+            "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+            "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+            "bright white bags contain 1 shiny gold bag.",
+            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+            "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+            "faded blue bags contain no other bags.",
+            "dotted black bags contain no other bags."
+        };
+
+        [Fact] Day07 GetInstance() => new Day07(_sampleInput);
+
+        [Fact] void Part1() => GetInstance().Part1().Should().Be(4);
+
+        [Fact] void Part2() => GetInstance().Part2().Should().Be(32);
+        
+        public static TheoryData<string, BagRule> ParseData = new()
+        {
+            {"light red bags contain 1 bright white bag, 2 muted yellow bags.",
+            new BagRule("light red", new[]{("bright white",1),("muted yellow",2)})},
+            
+            {"bright white bags contain 1 shiny gold bag.",
+            new BagRule("bright white", new[]{("shiny gold",1)})},
+            
+            {"faded blue bags contain no other bags.",
+            new BagRule("faded blue", Array.Empty<(string,int)>())}
+        };
+
+        [Theory, MemberData(nameof(ParseData))]
+        public void Parse(string input, BagRule expected)
+        {
+            var parsed = BagRule.Parse(input);
+            parsed.Color.Should().Be(expected.Color);
+            parsed.CanContain.Should().BeEquivalentTo(expected.CanContain);
+        }
+    }
+}

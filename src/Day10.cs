73bea3b634d55e72e69e049a277557311616729d
andi0b <andix25@gmail.com>
2020-12-10 11:51:38 +0100

    Day 10, Part 1

---
+++ src/Day10.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace aoc_runner
+{
+    public record Day10(int[] Adapters)
+    {
+        public int Part1() =>
+            Adapters.Concat(new[] {Adapters.Max() + 3})
+                    .OrderBy(x=>x)
+                    .Aggregate((prev: 0, diff1: 0, diff3: 0),
+                               (acc, next) => (next - acc.prev) switch
+                               {
+                                   1 => (next, acc.diff1 + 1, acc.diff3),
+                                   3 => (next, acc.diff1, acc.diff3 + 1),
+                                   _ => (next, acc.diff1, acc.diff3)
+                               },
+                               acc => acc.diff1 * acc.diff3);
+                 
+
+
+        public int Part2()=>0;
+    }
+}
---
+++ tests/Day10Test.cs
@@ -0,0 +1,18 @@
+﻿using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day10Test
+    {
+        [Fact]
+        Day10 GetInstance() => new Day10(new[]
+        {
+            28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3,
+        });
+
+        [Fact] void Part1() => GetInstance().Part1().Should().Be(220);
+        [Fact] void Part2() => GetInstance().Part2().Should().Be(19208);
+    }
+}

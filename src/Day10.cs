85c371298e4a06dbdacfc8bff3c818849d71bf06
andi0b <andix25@gmail.com>
2020-12-10 14:56:13 +0100

    Day 10: Just one Aggregate call per part

--- src/Day10.cs
+++ src/Day10.cs
@@ -1,51 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net.NetworkInformation;
+using System.Threading;
 
 namespace aoc_runner
 {
-    public class Day10
+    public record Day10(int[] Input)
     {
-        private readonly int[] _adapters;
-
-        public Day10(int[] input)
-        {
-            _adapters = input.Concat(new[] {0, input.Max() + 3})
-                            .OrderBy(x => x)
-                            .ToArray();
-        }
-
+        private readonly int[] _adapterJolts =
+            Input.Append(Input.Max() + 3)
+                 .OrderBy(x => x)
+                 .ToArray();
+        
         public int Part1() =>
-            _adapters
-               .Skip(1) // skip socket, as we use it as seed for aggregate
-               .Aggregate((prev: 0, diff1: 0, diff3: 0),
-                               (acc, next) => (next - acc.prev) switch
-                               {
-                                   1 => (next, acc.diff1 + 1, acc.diff3),
-                                   3 => (next, acc.diff1, acc.diff3 + 1),
-                                   _ => throw new ArgumentOutOfRangeException()
-                               },
-                               acc => acc.diff1 * acc.diff3);
+            _adapterJolts.Aggregate(
+                (prev: 0, diff1: 0, diff3: 0),
+                (acc, next) => (next - acc.prev) switch
+                {
+                    1 => (next, acc.diff1 + 1, acc.diff3),
+                    3 => (next, acc.diff1, acc.diff3 + 1),
+                    _ => throw new ArgumentOutOfRangeException()
+                },
+                acc => acc.diff1 * acc.diff3);
 
-
-        public long Part2()
-        {
-            var rightPossibilities = new long[_adapters.Length];
-            rightPossibilities[_adapters.Length - 1] = 1;
-
-            for (var i = _adapters.Length - 2; i >= 0; i--)
-            {
-                rightPossibilities[i] = DirectRightPossibilitiesIndices(i).Select(x => rightPossibilities[x]).Sum();
-            }
-
-            return rightPossibilities[0];
-        }
-
-        private IEnumerable<int> DirectRightPossibilitiesIndices(int index) =>
-            from j in Enumerable.Range(index + 1, 3)
-            where j < _adapters.Length
-            where _adapters[j] - _adapters[index] <= 3
-            select j;
+        public long Part2() =>
+            _adapterJolts.Aggregate(
+                new[] {(jolts: 0, count: 1L)},
+                (acc, nextJolts) =>
+                    acc[^Math.Min(2, acc.Length)..]
+                       .Append((
+                                   jolts: nextJolts,
+                                   count: acc.Where(prev => nextJolts - prev.jolts <= 3).Sum(x => x.count)
+                               ))
+                       .ToArray(),
+                acc => acc.Last().count);
     }
 }

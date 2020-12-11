d14b560ccb2a65ed36fbdcd258d9bd3cf51c9eec
andi0b <andix25@gmail.com>
2020-12-11 11:57:38 +0100

    Day 11 Part 1, very slow

---
+++ src/Day11.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace aoc_runner
+{
+    public record Day11(Day11.SeatMap Seats)
+    {
+        public Day11(string[] input) : this(SeatMap.Parse(input))
+        {
+        }
+
+        public int Part1()
+        {
+            var (permutation, isdifferent) = Seats.NextPermutation();
+            for (var i = 1;; i++)
+            {
+                if (!isdifferent) return permutation.Map.OfType<SeatType>().Count(x=>x == SeatType.Taken);
+                (permutation, isdifferent) = permutation.NextPermutation();
+            }
+        }
+        
+
+        public record SeatMap(SeatType[,] Map, int Width, int Height)
+        {
+            public (SeatMap permutation, bool isdifferent) NextPermutation()
+            {
+                var newMap = new SeatType[Width, Height];
+
+                var isDifferent = false;
+
+                for (var x = 0; x < Width; x++)
+                for (var y = 0; y < Height; y++)
+                {
+                    newMap[x, y] = Map[x, y] switch
+                    {
+                        SeatType.Empty when AdjacentSeats(x, y).All(x => x != SeatType.Taken) => SeatType.Taken,
+                        SeatType.Taken when AdjacentSeats(x, y).Count(x => x == SeatType.Taken) >= 4 => SeatType.Empty,
+                        var seatType => seatType
+                    };
+
+                    if (newMap[x, y] != Map[x, y])
+                        isDifferent = true;
+                }
+
+                return (this with{Map = newMap}, isDifferent);
+            }
+
+            public IEnumerable<SeatType> AdjacentSeats(int x, int y) =>
+                from xOff in new[] {-1, 0, 1}
+                from yOff in new[] {-1, 0, 1}
+                where !(xOff == 0 && yOff == 0)
+                let adjX = xOff + x
+                let adjY = yOff + y
+                where adjX >= 0 && adjX < Width
+                               && adjY >= 0 && adjY < Height
+                select Map[adjX, adjY];
+            
+            public static SeatMap Parse(string[] input)
+            {
+                var width = input[0].Length;
+                var height = input.Length;
+
+                var map = new SeatType[width, height];
+
+                for (var x = 0; x < width; x++)
+                for (var y = 0; y < height; y++)
+                    map[x, y] = Parse(input[y][x]);
+
+                return new(map, width, height);
+            }
+
+            static SeatType Parse(char c) => c switch
+            {
+                '#' => SeatType.Taken,
+                'L' => SeatType.Empty,
+                _ => SeatType.Floor
+            };
+
+            public override string ToString()
+            {
+                var sb = new StringBuilder();
+                for (var y = 0; y < Height; y++)
+                {
+                    for (var x = 0; x < Width; x++)
+                    {
+                        sb.Append(Map[x, y]switch
+                        {
+                            SeatType.Empty => 'L',
+                            SeatType.Taken => '#',
+                            _ => '.'
+                        });
+                    }
+
+                    if (y != Height - 1) sb.AppendLine();
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        public enum SeatType
+        {
+            Floor,
+            Empty,
+            Taken
+        };
+    }
+}
---
+++ tests/Day11Test.cs
@@ -0,0 +1,55 @@
+﻿using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day11Test
+    {
+        private string[] _input = {
+            "L.LL.LL.LL",
+            "LLLLLLL.LL",
+            "L.L.L..L..",
+            "LLLL.LL.LL",
+            "L.LL.LL.LL",
+            "L.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLLL",
+            "L.LLLLLL.L",
+            "L.LLLLL.LL",
+        };
+
+        [Fact] Day11 GetInstance() => new Day11(_input);
+
+        [Fact] void Part1() => GetInstance().Part1().Should().Be(37);
+
+
+        [Fact]
+        private void FirstRound() => GetInstance().Seats.NextPermutation().permutation.ToString().Should().Be(
+            @"#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##"
+        );
+        
+        [Fact]
+        private void SecondRound() => GetInstance().Seats.NextPermutation().permutation.NextPermutation().permutation.ToString().Should().Be(
+            @"#.LL.L#.##
+#LLLLLL.L#
+L.L.L..L..
+#LLL.LL.L#
+#.LL.LL.LL
+#.LLLL#.##
+..L.L.....
+#LLLLLLLL#
+#.LLLLLL.L
+#.#LLLL.##"
+        );
+    }
+}

d7279aef661ac3adcddb2c3a664b58d2bee10e11
andi0b <andix25@gmail.com>
2020-12-12 12:06:30 +0100

    Day 12, Part 1

---
+++ src/Day12.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace aoc_runner
+{
+    public record Day12((char, int)[] Instructions)
+    {
+        public Day12(string[] input) : this(input.Select(i => (i[0], int.Parse(i[1..]))).ToArray())
+        {
+        }
+
+        public int Part1() => Instructions.Aggregate(new ShipPosition(), (acc,nextInstruction) => acc.NextPosition(nextInstruction), acc=>acc.ManhattanDistanceTo());
+        public int Part2() => 1;
+
+        record ShipPosition(int X = 0, int Y = 0, int HeadingDegrees = 90)
+        {
+            public ShipPosition NextPosition((char, int) instruction) => instruction switch
+            {
+                ('R', { } degrees) => this with {HeadingDegrees = (HeadingDegrees + degrees) % 360},
+                ('L', { } degrees) => this with {HeadingDegrees = (360 + HeadingDegrees - degrees) % 360},
+                ('F', { } distance) => Travel(HeadingDirection, distance),
+                ({ } direction, { } distance) => Travel(direction, distance)
+            };
+
+            private char HeadingDirection => HeadingDegrees switch
+            {
+                0 => 'N',
+                90 => 'E',
+                180 => 'S',
+                270 => 'W'
+            };
+
+            public ShipPosition Travel(char direction, int distance) => direction switch
+            {
+                'N' => this with{Y = Y - distance},
+                'S' => this with{Y = Y + distance},
+                'W' => this with{X = X - distance},
+                'E' => this with{X = X + distance},
+            };
+
+            public int ManhattanDistanceTo(int x = 0, int y = 0) => Math.Abs(X - x) + Math.Abs(Y - y);
+        }
+    }
+}
---
+++ tests/Day12Test.cs
@@ -0,0 +1,22 @@
+﻿using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day12Test
+    {
+        private string[] input =
+        {
+            "F10",
+            "N3",
+            "F7",
+            "R90",
+            "F11",
+        };
+
+        [Fact] Day12 GetInstance() => new Day12(input);
+
+        [Fact] void Part1() => GetInstance().Part1().Should().Be(25);
+    }
+}

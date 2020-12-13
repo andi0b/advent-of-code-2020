bd3e18f115ea75b98ae08a1ccfe89ab7bda4515d
andi0b <andix25@gmail.com>
2020-12-13 11:30:54 +0100

    Day 13, Part 1

---
+++ src/Day13.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace aoc_runner
+{
+    public record Day13(int Earliest, int[] BusNumbers)
+    {
+        public Day13(params string[] input):this(
+            int.Parse(input[0]),
+            input[1].Split(',').Select(x=>int.TryParse(x, out var parsed) ? parsed : 0).ToArray()) { }
+
+        public int Part1()
+        {
+
+            var departures =
+                from bn in BusNumbers
+                where bn > 0
+                select (departureTime: NextDepartureTime(Earliest, bn), bn);
+
+            var (departureTime, busNumber) = departures.Min();
+            return (departureTime - Earliest) * busNumber;
+        }
+        
+        private int NextDepartureTime(int afterTimestamp, int busNumber) => (afterTimestamp / busNumber + 1) * busNumber;
+
+        public long Part2() => 0;
+    }
+}
---
+++ tests/Day13Test.cs
@@ -0,0 +1,23 @@
+﻿using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day13Test
+    {
+        [Fact] public Day13 GetInstance() => new Day13("939", "7,13,x,x,59,x,31,19");
+
+        [Fact] public void Part1() => GetInstance().Part1().Should().Be(295);
+
+        [Theory,
+         InlineData("7,13,x,x,59,x,31,19", 1068781),
+         InlineData("17,x,13,19", 3417),
+         InlineData("67,7,59,61", 754018),
+         InlineData("67,x,7,59,61", 779210),
+         InlineData("67,7,x,59,61", 1261476),
+         InlineData("1789,37,47,1889", 1202161486)]
+        public void Part2(string input, long expected) =>
+            new Day13("", input).Part2().Should().Be(expected);
+    }
+}

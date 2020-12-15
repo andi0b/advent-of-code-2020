6cb55b8a3df18ef55f1414ebaef8dd5eaa1d0abb
andi0b <andix25@gmail.com>
2020-12-15 15:09:22 +0100

    Day 15

---
+++ src/Day15.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc_runner
+{
+    public record Day15(int[] StartingNumbers)
+    {
+        public Day15() : this(new[] {11,18,0,20,1,7,16}) { }
+
+        public int Part1() => SolveFast(2020);
+        public int Part2() => SolveFast(30000000);
+        
+        public int Solve(int num)
+        {
+            var spokenNumbers = new List<int>(num);
+            spokenNumbers.AddRange(StartingNumbers);
+
+            int EndOffset(int num) => (
+                from x in spokenNumbers!.AsEnumerable().Reverse().Skip(1).Select((spokenNumber, n) => (spokenNumber, n))
+                where x.spokenNumber == num
+                select x.n + 1
+            ).FirstOrDefault();
+
+            while (spokenNumbers.Count < num)
+            {
+                spokenNumbers.Add(EndOffset(spokenNumbers.Last()));
+            }
+
+            return spokenNumbers.Last();
+        }
+
+        public int SolveFast(int num)
+        {
+            var spokenNumbers = StartingNumbers.Take(StartingNumbers.Length - 1)
+                                               .Select((spokenNumber, n) => (spokenNumber, n))
+                                               .ToDictionary(x => x.spokenNumber, x => x.n);
+
+            var lastSpokenNumber = StartingNumbers.Last();
+            for (var i = StartingNumbers.Length - 1; i <= num-2; i++)
+            {
+                var nextNumber =
+                    spokenNumbers.TryGetValue(lastSpokenNumber, out var previouslySpokenRound)
+                        ? i  - previouslySpokenRound
+                        : 0;
+                
+                spokenNumbers[lastSpokenNumber] = i;
+
+                lastSpokenNumber = nextNumber;
+            }
+
+            return lastSpokenNumber;
+        }
+    }
+}
---
+++ tests/Day15Test.cs
@@ -0,0 +1,20 @@
+﻿using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day15Test
+    {
+        [Theory,
+         InlineData(new[] {0, 3, 6}, 436),
+         InlineData(new[] {1, 3, 2}, 1),
+         InlineData(new[] {2, 1, 3}, 10),
+         InlineData(new[] {1, 2, 3}, 27),
+         InlineData(new[] {2, 3, 1}, 78),
+         InlineData(new[] {3, 2, 1}, 438),
+         InlineData(new[] {3, 1, 2}, 1836),
+        ]
+        void Part1(int[] startingNumbers, int expected) => new Day15(startingNumbers).Part1().Should().Be(expected);
+    }
+}

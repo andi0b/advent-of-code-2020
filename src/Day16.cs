8fa6f06fd70527781bcdeda37686e2d5b9557323
andi0b <andix25@gmail.com>
2020-12-16 12:35:57 +0100

    Day 16, Part 1

---
+++ src/Day16.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace aoc_runner
+{
+    public record Day16(Day16.Rule[] Rules, int[] MyTicket, int[][] NearbyTickets)
+    {
+        public Day16(string input) : this(Parse(input)) { }
+
+
+        public int Part1() => (
+            from ticket in NearbyTickets
+            from number in ticket
+            where !Rules.Any(r => r.InRanges(number))
+            select number
+        ).Sum();
+        
+        private static Day16 Parse(string input)
+        {
+            var ruleMatches = Regex.Matches(input, @"(.*): (\d*)-(\d*) or (\d*)-(\d*)");
+            var myTicketMatch = Regex.Match(input, @"your ticket:(?:\r\n|\n|\r)(.*)(?:\r)?", RegexOptions.Multiline);
+            var nearbyTicketsMatch = Regex.Match(input, @"nearby tickets:(?:\r\n|\n|\r)(.*)", RegexOptions.Multiline | RegexOptions.Singleline);
+
+            var rules = (
+                from m in ruleMatches
+                let range1 = new Range(int.Parse(m.Groups[2].Value), int.Parse(m.Groups[3].Value))
+                let range2 = new Range(int.Parse(m.Groups[4].Value), int.Parse(m.Groups[5].Value))
+                select new Rule(m.Groups[1].Value, range1, range2)
+            ).ToArray();
+
+            int[] ParseTicket(string ti) => ti.Split(',').Select(int.Parse).ToArray();
+            
+            var myTicket = ParseTicket(myTicketMatch.Groups[1].Value);
+
+            var nearbyTickets = nearbyTicketsMatch.Groups[1].Value.Split(Environment.NewLine).Select(ParseTicket).ToArray();
+
+            return new (rules, myTicket, nearbyTickets);
+        }
+
+        public record Rule(string Name, Range Range1, Range Range2)
+        {
+            public Range[] Ranges => new[] {Range1, Range2};
+            public bool InRanges(int num) => Range1.InRange(num) || Range2.InRange(num);
+        }
+
+        public record Range(int From, int To)
+        {
+            public bool InRange(int num) => num >= From && num <= To;
+        }
+    }
+}
---
+++ tests/Day16Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day16Test
+    {
+        private readonly string _demoInput = @"class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12";
+
+        [Fact]
+        void Parse()
+        {
+            var (rules, myTicket, nearbyTickets) = new Day16(_demoInput);
+
+            rules.Should().BeEquivalentTo(new Day16.Rule[]
+            {
+                new("class", new(1, 3), new(5, 7)),
+                new("row", new(6, 11), new(33, 44)),
+                new("seat", new(13, 40), new(45, 50))
+            });
+            
+            myTicket.Should().BeEquivalentTo(new[] {7, 1, 14});
+
+            nearbyTickets.Should().BeEquivalentTo(new int[][]
+            {
+                new[] {7, 3, 47},
+                new[] {40, 4, 50},
+                new[] {55, 2, 20},
+                new[] {38, 6, 12}
+            });
+        }
+
+        [Fact]
+        void Part1() => new Day16(_demoInput).Part1().Should().Be(71);
+    }
+}

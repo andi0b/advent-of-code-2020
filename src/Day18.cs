46a425bfdeec656a9f77782c1e5f2fe4f1acaf1e
andi0b <andix25@gmail.com>
2020-12-20 16:31:35 +0100

    Day 18, ugly implemementation

--- src/Day18.cs
+++ src/Day18.cs
@@ -1,19 +1,108 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace aoc_runner
 {
     public class Day18
     {
-        public static int Solve(string input)
-        {
-        }
-        
-        
+        public long Part1(string[] input) =>
+            input.Select(x => Expression.Parse(x).Solve())
+                 .Sum();
+
+        public long Part2(string[] input) =>
+            input.Select(ParenthesisHack)
+                 .Select(x => Expression.Parse(x).Solve())
+                 .Sum();
 
         
-        
-        
-        private static string ParanthesisHack(string input)
-            => "((" + input.Replace("+", ")+(").Replace("*", "))*((") + "))";
+
+        public record Expression(object[] Items)
+        {
+            public long Solve()
+            {
+                var lhs = (long?) null;
+                var op = ' ';
+                foreach (var item in Items)
+                {
+                    long GetValue()=>item switch
+                    {
+                        (long num) => num,
+                        (Expression expression) => expression.Solve(),
+                        _ => throw new ArgumentOutOfRangeException()
+                    };
+                    
+                    if (lhs == null)
+                    {
+                        lhs = GetValue();
+                    }
+                    else if (item is char c)
+                    {
+                        op = c;
+                    }
+                    else
+                    {
+                        if (op == '+')
+                            lhs += GetValue();
+                        else if (op == '*')
+                            lhs *= GetValue();
+                    }
+                }
+
+                return lhs.Value;
+            }
+
+            public static Expression Parse(string input)
+            {
+                var items = new List<object>();
+                
+                var numbers = new List<char>();
+                var insideBracket = new List<char>();
+                
+                var bracketCount = 0;
+
+                foreach (var c in input.Append(' '))
+                {
+                    if (c == '(')
+                        bracketCount++;
+
+                    if (!char.IsDigit(c) && numbers.Any())
+                    {
+                        items.Add(long.Parse(new string(numbers.ToArray())));
+                        numbers.Clear();
+                    }
+                    else if (c == ')' && bracketCount == 1)
+                    {
+                        items.Add(Parse(new string(insideBracket.ToArray())));
+                        insideBracket.Clear();
+                    }
+                    else if (bracketCount == 1 && c == '(')
+                        continue;
+                    else if (bracketCount > 0)
+                        insideBracket.Add(c);
+                    else if (char.IsDigit(c))
+                        numbers.Add(c);
+                    else if (c == ' ')
+                        continue;
+                    else
+                        items.Add(c);
+
+                    if (c == ')')
+                        bracketCount--;
+                }
+
+                return new(items.ToArray());
+            }
+        }
+
+
+        public static string ParenthesisHack(string input)
+            => "((" +
+               input.Replace("(", "((")
+                    .Replace(")", "))")
+                    .Replace("+", ") + (")
+                    .Replace("*", ")) * ((")
+               + "))";
     }
 }
--- tests/Day18Test.cs
+++ tests/Day18Test.cs
@@ -7,12 +7,25 @@ namespace tests
     public class Day18Test
     {
         [Theory,
+         InlineData("11 + 22 * 10", 330),
          InlineData("1 + 2 * 3 + 4 * 5 + 6", 71),
          InlineData("1 + (2 * 3) + (4 * (5 + 6))", 51),
          InlineData("2 * 3 + (4 * 5)", 26),
          InlineData("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437),
          InlineData("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240),
          InlineData("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
-        public void Solve(string input, int expected) => Day18.Solve(input).Should().Be(expected);
+        public void Solve(string input, int expected) => Day18.Expression.Parse(input).Solve().Should().Be(expected);
+        
+        
+        [Theory,
+         InlineData("11 + 22 * 10", 330),
+         InlineData("1 + 2 * 3 + 4 * 5 + 6", 231),
+         InlineData("2 * 3 + (4 * 5)", 46),
+         InlineData("1 + (2 * 3) + (4 * (5 + 6))", 51),
+         InlineData("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445),
+         InlineData("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060),
+         InlineData("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340)]
+        public void Solve_Part2(string input, int expected) => Day18.Expression.Parse(Day18.ParenthesisHack(input)).Solve().Should().Be(expected);
+
     }
 }

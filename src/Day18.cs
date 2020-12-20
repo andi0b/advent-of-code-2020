ae39195d478294191d8165aa51320ec592c48cf0
andi0b <andix25@gmail.com>
2020-12-20 09:32:42 +0100

    day 18 tests

---
+++ src/Day18.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+namespace aoc_runner
+{
+    public class Day18
+    {
+        public static int Solve(string input)
+        {
+        }
+        
+        
+
+        
+        
+        
+        private static string ParanthesisHack(string input)
+            => "((" + input.Replace("+", ")+(").Replace("*", "))*((") + "))";
+    }
+}
---
+++ tests/Day18Test.cs
@@ -0,0 +1,18 @@
+﻿using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day18Test
+    {
+        [Theory,
+         InlineData("1 + 2 * 3 + 4 * 5 + 6", 71),
+         InlineData("1 + (2 * 3) + (4 * (5 + 6))", 51),
+         InlineData("2 * 3 + (4 * 5)", 26),
+         InlineData("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437),
+         InlineData("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240),
+         InlineData("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
+        public void Solve(string input, int expected) => Day18.Solve(input).Should().Be(expected);
+    }
+}

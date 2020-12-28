e68755af2f71f8a1d06427d7a6f9cca16c11c202
andi0b <andix25@gmail.com>
2020-12-28 12:15:18 +0100

    day 20, cleanup

--- src/Day20.cs
+++ src/Day20.cs
@@ -8,21 +8,26 @@ using System.Text.RegularExpressions;
 
 namespace aoc_runner
 {
-    public record Day20(Day20.Tile[] Tiles)
+    public class Day20
     {
-        public Day20(string input) 
-            : this(input.Split(Environment.NewLine+Environment.NewLine).Select(Tile.Parse).ToArray()) { }
+        private readonly Tile[] _allPermutations;
 
-        private readonly Tile[] _allPermutations = Tiles.SelectMany(x => x.AllPermutations()).ToArray();
+        private readonly Tile[] _topLeftCornerTiles;
 
-        
+        public Day20(string input)
+        {
+            var tiles = input.Split(Environment.NewLine + Environment.NewLine).Select(Tile.Parse).ToArray();
+
+            _allPermutations = tiles.SelectMany(x => x.AllPermutations()).ToArray();
+
+            _topLeftCornerTiles = _allPermutations.Where(i => !_allPermutations.Any(j => j.RightId == i.LeftId && j.TileId != i.TileId) &&
+                                                              !_allPermutations.Any(j => j.BottomId == i.TopId && j.TileId != i.TileId))
+                                                  .ToArray();
+        }
+
         public long Part1()
         {
-            var cornerTiles = _allPermutations.Where(i => !_allPermutations.Any(j => j.RightId == i.LeftId && j.TileId != i.TileId) &&
-                                                          !_allPermutations.Any(j => j.TopId == i.BottomId && j.TileId != i.TileId))
-                                              .ToArray();
-
-            var ids = cornerTiles.Select(x => x.TileId).Distinct().ToArray();
+            var ids = _topLeftCornerTiles.Select(x => x.TileId).Distinct().ToArray();
 
             Debug.Assert(ids.Length == 4);
 
@@ -31,63 +36,69 @@ namespace aoc_runner
         
         public long Part2()
         {
-            
-            var topLeftCornerTiles = _allPermutations.Where(i => !_allPermutations.Any(j => j.RightId == i.LeftId && j.TileId != i.TileId) &&
-                                                                !_allPermutations.Any(j => j.BottomId == i.TopId && j.TileId != i.TileId))
-                                                    .ToArray();
-
-            foreach (var tlct in topLeftCornerTiles)
+            foreach (var tlct in _topLeftCornerTiles)
             {
-                var placedTiles = PlaceTiles(tlct);
-
-                var gridWith = placedTiles.Keys.Max(i => i.x) + 1;
-                var gridHeight = placedTiles.Keys.Max(i => i.y) + 1;
-                
-                char Grid(int x, int y) => placedTiles![(x / 8, y / 8)].Pixels[y % 8 + 1][x % 8 + 1];
-
-                var seaMonster = new[]
-                {
-                    "                  # ".ToArray(),
-                    "#    ##    ##    ###".ToArray(),
-                    " #  #  #  #  #  #   ".ToArray()
-                };
-
-                bool IsSeaMonsterAt((int x, int y) location)
-                {
-                    for (var y = 0; y < 3; y++)
-                    for (var x = 0; x < 20; x++)
-                    {
-                        if (seaMonster![y][x] == ' ') continue;
-                        if (Grid(location.x + x, location.y + y) != '#') return false;
-                    }
-
-                    return true;
-                }
-
-                var seaMonsterLocations =
-                    from x in Enumerable.Range(0, gridWith * 8 - 20)
-                    from y in Enumerable.Range(0, gridHeight * 8 - 3)
-                    let potentialLocation = (x, y)
-                    where IsSeaMonsterAt(potentialLocation)
-                    select potentialLocation;
-
-                var seaMonsterCount = seaMonsterLocations.Count();
-
-                if (seaMonsterCount > 0)
-                {
-                    var roughness = 0; 
-                    
-                    for (var y = 0; y < gridHeight*8; y++)
-                    for (var x = 0; x < gridWith * 8; x++)
-                    {
-                        if (Grid(x, y) == '#') roughness++;
-                    }
-
-                    return roughness - seaMonsterCount * 15;
-                }
+                var roughness = CalculateRoughness(tlct);
+                if (roughness > 0) return roughness;
             }
 
-            return -1;
+            throw new Exception("No sea monster found!");
+        }
+
+        private int CalculateRoughness(Tile topLeftCornerTile)
+        {
+            var placedTiles = PlaceTiles(topLeftCornerTile);
+
+            var gridWith = placedTiles.Keys.Max(i => i.x) + 1;
+            var gridHeight = placedTiles.Keys.Max(i => i.y) + 1;
+
+            char Grid(int x, int y) => placedTiles![(x / 8, y / 8)].Pixels[y % 8 + 1][x % 8 + 1];
+
+            var seaMonster = new[]
+            {
+                "                  # ".ToArray(),
+                "#    ##    ##    ###".ToArray(),
+                " #  #  #  #  #  #   ".ToArray()
+            };
+            const int seaMonsterWith = 20;
+            const int seaMonsterHeight = 3;
+            const int seaMonsterTiles = 15;
+
+            bool IsSeaMonsterAt((int x, int y) location)
+            {
+                for (var y = 0; y < 3; y++)
+                for (var x = 0; x < 20; x++)
+                {
+                    if (seaMonster![y][x] == ' ') continue;
+                    if (Grid(location.x + x, location.y + y) != '#') return false;
+                }
+
+                return true;
+            }
+
+            var seaMonsterLocations =
+                from x in Enumerable.Range(0, gridWith * 8 - seaMonsterWith)
+                from y in Enumerable.Range(0, gridHeight * 8 - seaMonsterHeight)
+                let potentialLocation = (x, y)
+                where IsSeaMonsterAt(potentialLocation)
+                select potentialLocation;
+
+            var seaMonsterCount = seaMonsterLocations.Count();
+
+            if (seaMonsterCount > 0)
+            {
+                var roughness = 0;
+
+                for (var y = 0; y < gridHeight * 8; y++)
+                for (var x = 0; x < gridWith * 8; x++)
+                {
+                    if (Grid(x, y) == '#') roughness++;
+                }
+
+                return roughness - seaMonsterCount * seaMonsterTiles;
+            }
+
+            return 0;
         }
 
         Dictionary<(int x, int y), Tile> PlaceTiles(Tile topLeftCorner)
@@ -138,7 +149,7 @@ namespace aoc_runner
             public int BottomId { get; } = GetId(Pixels, z => (z, Size - 1));
             public int LeftId { get; } = GetId(Pixels, z => (0, z));
             public int RightId { get; } = GetId(Pixels, z => (Size - 1, z));
-
+            
             private static int GetId(char[][] pixels, Func<int, (int x, int y)> selectorFunc) => (
                 from z in Enumerable.Range(0, Size)
                 let coordinates = selectorFunc(z)
@@ -146,7 +157,11 @@ namespace aoc_runner
                 select (pixel == '#' ? 1 : 0) << z
             ).Aggregate(0, (acc, next) => acc | next);
 
-            public Tile Transform(Action<int, int, char[][]> transformFunc)
+            public Tile TurnClockwise() => Transform((x, y, newPixels) => newPixels[x][Size - 1 - y] = Pixels[y][x]);
+
+            public Tile Flip() => Transform((x, y, newPixels) => newPixels[y][Size - 1 - x] = Pixels[y][x]);
+
+            private Tile Transform(Action<int, int, char[][]> transformFunc)
             {
                 var newPixels = Enumerable.Repeat(false, Size).Select(_ => new char[Size]).ToArray();
 
@@ -159,9 +174,6 @@ namespace aoc_runner
                 return new(TileId, newPixels);
             }
 
-            public Tile TurnClockwise() => Transform((x, y, newPixels) => newPixels[x][Size - 1 - y] = Pixels[y][x]);
-            public Tile Flip() => Transform((x, y, newPixels) => newPixels[y][Size - 1 - x] = Pixels[y][x]);
-
             public IEnumerable<Tile> AllPermutations()
             {
                 var tile = this;
@@ -170,7 +182,7 @@ namespace aoc_runner
                     yield return tile;
 
                     if (i == 4)
-                        tile = this.Flip();
+                        tile = Flip();
                     else if (i == 8)
                         yield break;
 
@@ -178,9 +190,9 @@ namespace aoc_runner
                 }
             }
 
-            public static Tile Parse(string ínput)
+            public static Tile Parse(string input)
             {
-                var lines = ínput.Split(Environment.NewLine);
+                var lines = input.Split(Environment.NewLine);
 
                 var pixels = lines.Skip(1).Select(l => l.ToArray()).ToArray();
 
@@ -193,12 +205,11 @@ namespace aoc_runner
             public string Format()
             {
                 var sb = new StringBuilder();
-                sb.AppendLine($"Tile {TileId}:");
+                sb.Append($"Tile {TileId}:");
+                
                 foreach (var line in Pixels)
-                    sb.AppendLine(new string(line));
+                    sb.Append(Environment.NewLine + new string(line));
 
-                var nlSize = Environment.NewLine.Length;
-                sb.Remove(sb.Length - nlSize, nlSize);
                 return sb.ToString();
             }
         }

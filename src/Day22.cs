207dd86a4519d0fa481373afb2e3eda6020f6b85
andi0b <andix25@gmail.com>
2020-12-29 17:45:08 +0100

    Day 22 Part 1

---
+++ src/Day22.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc_runner
+{
+    public class Day22
+    {        
+        public int Part1(string input)
+        {
+            var decks = Parse(input);
+
+            while (!decks.Any(x => x.IsEmpty))
+            {
+                var ordered = decks.OrderBy(x => x.Top).ToArray();
+                var winner = ordered.Last();
+                var loser = ordered.First();
+
+                winner.Put(winner.Take(), loser.Take());
+            }
+
+            return decks.Single(x => !x.IsEmpty).CalculateScore();
+        }
+
+        public PlayerDeck[] Parse(string input)
+        {
+            PlayerDeck ParseDeck(string i) => new(i.Split(Environment.NewLine).Skip(1).Select(int.Parse));
+                
+            var playerParts = input.Split(Environment.NewLine + Environment.NewLine);
+            return new[]
+            {
+                ParseDeck(playerParts[0]),
+                ParseDeck(playerParts[1])
+            };
+        }
+
+        public class PlayerDeck
+        {
+            private readonly Queue<int> _cards;
+
+            public PlayerDeck(IEnumerable<int> cards) => _cards = new(cards);
+
+            public int Take() => _cards.Dequeue();
+
+            public void Put(int winner, int loser)
+            {
+                _cards.Enqueue(winner);
+                _cards.Enqueue(loser);
+            }
+
+            public int? Top => _cards.TryPeek(out var card) ? card : null;
+            
+            public bool IsEmpty => !_cards.TryPeek(out _);
+
+            public int CalculateScore() => _cards.Reverse().Select((card, n) => card * (n+1)).Sum();
+        }
+    }
+
+
+}
---
+++ tests/Day22Test.cs
@@ -0,0 +1,26 @@
+﻿using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day22Test
+    {
+        private static string _example = @"Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10";
+
+        [Fact]
+        void Part1() => new Day22().Part1(_example).Should().Be(306);
+    }
+}

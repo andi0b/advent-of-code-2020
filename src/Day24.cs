5a83c9e631d03802f9e7e90bfaa05a29e86ada3a
andi0b <andix25@gmail.com>
2020-12-30 21:43:26 +0100

    day 24 Part 1

---
+++ src/Day24.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc_runner
+{
+    public class Day24
+    {
+
+        public int Part1(string[] input) => (
+            from move in input.Select(ParseMoves)
+            group move by move
+        ).Count(x => x.Count() % 2 != 0);
+
+        public static (int x, int y) ParseMoves(string input) => (
+            from pair in input.Prepend(' ').Zip(input)
+            select pair switch
+            {
+                ('s', 'e') => (+1, -1),
+                ('s', 'w') => (+0, -1),
+                ('n', 'e') => (+0, +1),
+                ('n', 'w') => (-1, +1),
+                (_, 'e') => (+1, +0),
+                (_, 'w') => (-1, +0),
+                _ => (0, 0)
+            }).Aggregate((0, 0), (acc, next) => (acc.Item1 + next.Item1, acc.Item2 + next.Item2));
+    }
+}
---
+++ tests/Day24Test.cs
@@ -0,0 +1,39 @@
+﻿using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day24Test
+    {
+        [Fact]
+        void Move1() => Day24.ParseMoves("nwwswee").Should().Be((0, 0));
+
+        [Fact]
+        void Part1() => new Day24().Part1(_sample).Should().Be(10);
+
+        private string[] _sample = new[]
+        {
+            "sesenwnenenewseeswwswswwnenewsewsw",
+            "neeenesenwnwwswnenewnwwsewnenwseswesw",
+            "seswneswswsenwwnwse",
+            "nwnwneseeswswnenewneswwnewseswneseene",
+            "swweswneswnenwsewnwneneseenw",
+            "eesenwseswswnenwswnwnwsewwnwsene",
+            "sewnenenenesenwsewnenwwwse",
+            "wenwwweseeeweswwwnwwe",
+            "wsweesenenewnwwnwsenewsenwwsesesenwne",
+            "neeswseenwwswnwswswnw",
+            "nenwswwsewswnenenewsenwsenwnesesenew",
+            "enewnwewneswsewnwswenweswnenwsenwsw",
+            "sweneswneswneneenwnewenewwneswswnese",
+            "swwesenesewenwneswnwwneseswwne",
+            "enesenwswwswneneswsenwnewswseenwsese",
+            "wnwnesenesenenwwnenwsewesewsesesew",
+            "nenewswnwewswnenesenwnesewesw",
+            "eneswnwswnwsenenwnwnwwseeswneewsenese",
+            "neswnwewnwnwseenwseesewsenwsweewe",
+            "wseweeenwnesenwwwswnew",
+        };
+    }
+}

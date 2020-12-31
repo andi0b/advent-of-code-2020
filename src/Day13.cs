88ed1c0825254f62131a9a4032d1fbde79017f34
andi0b <andix25@gmail.com>
2020-12-31 13:45:37 +0100

    Day 25, long all the way, seems to be faster

--- src/Day13.cs
+++ src/Day13.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Numerics;
 
 namespace aoc_runner
 {
@@ -21,7 +22,38 @@ namespace aoc_runner
         }
         
         private int NextDepartureTime(int afterTimestamp, int busNumber) => (afterTimestamp / busNumber + 1) * busNumber;
+        
+        
+        
+        public object Part2(string input) =>
+            ChineseRemainderTheorem(
+                Parse(input).buses
+                            .Select(bus => (mod: bus.period, a: bus.period - bus.delay))
+                            .ToArray()
+            );
 
-        public long Part2() => 0;
+        (int earliestDepart, (long period, int delay)[] buses) Parse(string input) {
+            var lines = input.Split("\n");
+            var earliestDepart = int.Parse(lines[0]);
+            var buses = lines[1].Split(",")
+                                .Select((part, idx) => (part, idx))
+                                .Where(item => item.part != "x")
+                                .Select(item => (period: long.Parse(item.part), delay: item.idx))
+                                .ToArray();
+            return (earliestDepart, buses);
+        }
+
+        // https://rosettacode.org/wiki/Chinese_remainder_theorem#C.23
+        long ChineseRemainderTheorem((long mod, long a)[] items) {
+            var prod = items.Aggregate(1L, (acc, item) => acc * item.mod);
+            var sum = items.Select((item, i) => {
+                var p = prod / item.mod;
+                return item.a * ModInv(p, item.mod) * p;
+            }).Sum();
+
+            return sum % prod;
+        }
+
+        long ModInv(long a, long m) => (long)BigInteger.ModPow(a, m - 2, m);
     }
 }
--- src/Day25.cs
+++ src/Day25.cs
@@ -7,19 +7,19 @@ namespace aoc_runner
 {
     public record Day25(int[] Input)
     {
-        public int Part1() => Transform(Input[1], BruteForceLoopSize(7, Input[0]));
+        public long Part1() => Transform(Input[1], BruteForceLoopSize(7, Input[0]));
 
         public static int BruteForceLoopSize(int subjectNumber, int transformationResult)
         {
-            var num = 1;
+            var num = 1L;
             for (var loopSize = 1;; loopSize++)
                 if (transformationResult == (num = Next(num, subjectNumber)))
                     return loopSize;
         }
 
-        public static int Next(int num, int subjectNumber) => (int) ((long) num * subjectNumber % 20201227);
+        public static long Next(long num, int subjectNumber) => num * subjectNumber % 20201227;
 
-        public static int Transform(int subjectNumber, int loopSize) =>
-            Enumerable.Repeat(1, loopSize).Aggregate(1, (acc, _) => Next(acc, subjectNumber));
+        public static long Transform(int subjectNumber, int loopSize) =>
+            Enumerable.Repeat(1, loopSize).Aggregate(1L, (acc, _) => Next(acc, subjectNumber));
     }
 }

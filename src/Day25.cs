35aadeb1b7e4287c705faad2970dfe1cebe27b90
andi0b <andix25@gmail.com>
2020-12-31 13:42:52 +0100

    Day 25, there is no Part 2, remove unnecessary code :(

--- src/Day25.cs
+++ src/Day25.cs
@@ -1,55 +1,25 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using MathNet.Numerics.Statistics;
 
 namespace aoc_runner
 {
     public record Day25(int[] Input)
     {
-
-        public int Part1()
-        {
-            var loopSize0 = BruteForceLoopSize(7, Input[0]);
-            return Transform(Input[1], loopSize0);
-        }
-
+        public int Part1() => Transform(Input[1], BruteForceLoopSize(7, Input[0]));
 
         public static int BruteForceLoopSize(int subjectNumber, int transformationResult)
         {
-            var num = 1L;
+            var num = 1;
             for (var loopSize = 1;; loopSize++)
-                if (transformationResult == (num = num * subjectNumber % 20201227))
+                if (transformationResult == (num = Next(num, subjectNumber)))
                     return loopSize;
         }
 
-        public static (int subjectNumber, int loopSize) BruteForce2(int transformationResult)
-        {
-            for (var subjectNumber = 1;; subjectNumber++)
-            {
-                for (var loopSize = 1; loopSize <= subjectNumber; loopSize++)
-                {
-                    if (Transform(subjectNumber, loopSize) == transformationResult) return (subjectNumber, loopSize);
-                }
+        public static int Next(int num, int subjectNumber) => (int) ((long) num * subjectNumber % 20201227);
 
-                for (var subjectNumber2 = 1; subjectNumber2 < subjectNumber; subjectNumber2++)
-                {
-                    if (Transform(subjectNumber2, subjectNumber) == transformationResult) return (subjectNumber2, subjectNumber);
-                }
-            }
-        }
-
-        public static int Transform2(int subjectNumber, int loopSize)
-        {
-            var num = 1L;
-            for (var i = 0; i < loopSize; i++)
-            {
-                num = num * subjectNumber % 20201227;
-            }
-
-            return (int) num;
-        }
-        
         public static int Transform(int subjectNumber, int loopSize) =>
-            Enumerable.Repeat(1, loopSize).Aggregate(1L, (acc, _) => acc * subjectNumber % 20201227, acc => (int) acc);
+            Enumerable.Repeat(1, loopSize).Aggregate(1, (acc, _) => Next(acc, subjectNumber));
     }
 }

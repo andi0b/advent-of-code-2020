b67c3ac19c5101f01edc21620214356fe60430dc
andi0b <andix25@gmail.com>
2020-12-31 13:02:29 +0100

    Day 25 Part 1

---
+++ src/Day25.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace aoc_runner
+{
+    public record Day25(int[] Input)
+    {
+
+        public int Part1()
+        {
+            var loopSize0 = BruteForceLoopSize(7, Input[0]);
+            return Transform(Input[1], loopSize0);
+        }
+
+
+        public static int BruteForceLoopSize(int subjectNumber, int transformationResult)
+        {
+            var num = 1L;
+            for (var loopSize = 1;; loopSize++)
+                if (transformationResult == (num = num * subjectNumber % 20201227))
+                    return loopSize;
+        }
+
+        public static (int subjectNumber, int loopSize) BruteForce2(int transformationResult)
+        {
+            for (var subjectNumber = 1;; subjectNumber++)
+            {
+                for (var loopSize = 1; loopSize <= subjectNumber; loopSize++)
+                {
+                    if (Transform(subjectNumber, loopSize) == transformationResult) return (subjectNumber, loopSize);
+                }
+
+                for (var subjectNumber2 = 1; subjectNumber2 < subjectNumber; subjectNumber2++)
+                {
+                    if (Transform(subjectNumber2, subjectNumber) == transformationResult) return (subjectNumber2, subjectNumber);
+                }
+            }
+        }
+
+        public static int Transform2(int subjectNumber, int loopSize)
+        {
+            var num = 1L;
+            for (var i = 0; i < loopSize; i++)
+            {
+                num = num * subjectNumber % 20201227;
+            }
+
+            return (int) num;
+        }
+        
+        public static int Transform(int subjectNumber, int loopSize) =>
+            Enumerable.Repeat(1, loopSize).Aggregate(1L, (acc, _) => acc * subjectNumber % 20201227, acc => (int) acc);
+    }
+}
---
+++ tests/Day25Test.cs
@@ -0,0 +1,26 @@
+﻿using aoc_runner;
+using FluentAssertions;
+using Xunit;
+
+namespace tests
+{
+    public class Day25Test
+    {
+        [Theory,
+         InlineData(17807724, 5764801),
+         InlineData(5764801, 17807724)]
+        public void Part1(int a, int b) => new Day25(new[] {a, b}).Part1().Should().Be(14897079);
+        
+        [Theory,
+         InlineData(7, 8, 5764801),
+         InlineData(7, 11, 17807724)]
+        public void Transform(int subjectNumber, int loopSize, int expected) =>
+            Day25.Transform(subjectNumber, loopSize).Should().Be(expected);
+
+        [Theory,
+         InlineData(5764801, 7, 8),
+         InlineData(17807724, 7, 11)]
+        public void BruteForce(int num, int subjectNumber, int loopSize) =>
+            Day25.BruteForceLoopSize(7, num).Should().Be(loopSize);
+    }
+}
